8621be28bc80104de42740bea4ea6cecc97678f8
vladpereskokov <vladpereskokov@mail.ru>
2017-04-13 19:03:56 +0300

    delete jsx

--- public/index.jsx
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Hello_1 = require("./components/Hello");
-ReactDOM.render(<Hello_1.Hello compiler="TypeScript" framework="React"/>, document.getElementById("example"));
