06c57fbcf96887785c1f9ad0c61ae35aa009386e
vladpereskokov <vladpereskokov@mail.ru>
2017-04-17 19:39:17 +0300

    add new form

--- public/components/Form/Form.tsx
+++ public/components/Form/Form.tsx
@@ -1,26 +1,52 @@
 import * as React from 'react';
+import {Link} from "react-router-dom";
 
 import './Form.scss';
+import {Button} from "../Button/Button";
 
 interface Props {
-
+  fields?: Array<any>;
 }
 
-interface States {
-  errorFields: Array<any>;
-}
-
-export class Form extends React.Component<Props, States> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      isActive: {}
-    }
+export class Form extends React.Component<Props, void> {
+  constructor() {
+    super();
   }
 
   render() {
+    const data = this.props.fields;
+
+    const content = data.map((item, index) => {
+      return (
+        <li key={index}>
+          <label>
+            { item.author }
+          </label>
+          <input name={item.name} type={item.type} placeholder={item.placeholder}/>
+          <span name="{input.name}">
+            { item.text }
+            </span>
+        </li>
+      )
+    });
+
     return (
-      <div>
+      <div className="form__wrapper-elements">
+        <div className="wrapper__form-center">
+          <div className="form__header">
+            <span>Soul Hunting</span>
+          </div>
+          <form className="form" name="{data.title}">
+            <span className="errorText__response">
+            </span>
+            <ul>
+              {content}
+            </ul>
+            <Link to="/">
+              <Button text="SIGN IN" isActive={true}/>
+            </Link>
+          </form>
+        </div>
       </div>
     );
   }
--- public/views/SignIn/SignIn.tsx
+++ public/views/SignIn/SignIn.tsx
@@ -2,8 +2,25 @@ import * as React from 'react';
 import {Link} from "react-router-dom";
 
 import {Button} from "../../components/Button/Button";
+import {Form} from "../../components/Form/Form";
 
 import './SignIn.scss';
+
+const obj = [
+  {
+    author: 'qwe',
+    type: 'text',
+    text: 'enter',
+    name: 'login',
+    placeholder: 'username'
+  }, {
+    author: 'qwe',
+    type: 'text',
+    text: 'enter',
+    name: 'login',
+    placeholder: 'username'
+  }
+];
 
 export class SignIn extends React.Component<void, void> {
   render() {
@@ -17,6 +34,7 @@ export class SignIn extends React.Component<void, void> {
         <div className='registration__back'>
         </div>
         <div className='registration'>
+          <Form fields={obj}/>
         </div>
       </div>
     );
