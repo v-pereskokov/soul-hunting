3d98386561b17ae9ccf4f9054f21829778f1f106
e.utrobin <e.utrobin@corp.mail.ru>
2017-04-20 22:57:59 +0300

    Подключили редакс опаопа

--- public/application.tsx
+++ public/application.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import {Router, Route, BrowserRouter} from 'react-router-dom';
 import {createBrowserHistory} from 'history';
+import { Provider } from 'react-redux';
 
-import {MainTemplate} from './teamplates/MainTemplate/MainTemplate';
+import MainTemplate from './teamplates/MainTemplate/MainTemplate';
 import {Home} from "./views/Home/Home";
 import {SignIn} from "./views/SignIn/SignIn";
 import {SignUp} from "./views/SignUp/SignUp";
@@ -14,17 +15,21 @@ import './static/css/reset.scss';
 import './static/css/fonts.scss';
 import './static/css/main.scss';
 
+import store from './modules/index';
+
 const history = createBrowserHistory();
 
 export const App = () => (
-  <BrowserRouter>
-    <Router history={ history }>
-      <MainTemplate>
-        <Route path={ RoutesMap.HOME } component={ Home }/>
-        <Route path={ RoutesMap.SIGNIN } component={ SignIn }/>
-        <Route path={ RoutesMap.SIGNUP } component={ SignUp }/>
-        <Route path={ RoutesMap.SCOREBOARD } component={ Scoreboard }/>
-      </MainTemplate>
-    </Router>
-  </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <Router history={ history }>
+          <MainTemplate>
+            <Route path={ RoutesMap.HOME } component={ Home }/>
+            <Route path={ RoutesMap.SIGNIN } component={ SignIn }/>
+            <Route path={ RoutesMap.SIGNUP } component={ SignUp }/>
+            <Route path={ RoutesMap.SCOREBOARD } component={ Scoreboard }/>
+          </MainTemplate>
+        </Router>
+      </BrowserRouter>
+    </Provider>
 );
--- public/teamplates/MainTemplate/MainTemplate.tsx
+++ public/teamplates/MainTemplate/MainTemplate.tsx
@@ -3,13 +3,15 @@ import * as React from 'react';
 import {Logo} from "../../components/Logo/Logo";
 import {PreLoader} from "../../components/PreLoader/PreLoader";
 import {Footer} from "../../components/Footer/Footer";
+import { connect } from 'react-redux';
 
 import './MainTemplate.scss';
 
-export class MainTemplate extends React.Component<void, void> {
+class MainTemplate extends React.Component<void, void> {
   render() {
     return (
       <div className='wrapper'>
+          {console.log(this.props.state)}
         <PreLoader />
         <Logo />
         { this.props.children }
@@ -18,3 +20,10 @@ export class MainTemplate extends React.Component<void, void> {
     );
   }
 }
+
+const mapStateToProps = state => {
+    return {state}
+};
+
+
+export default connect(mapStateToProps)(MainTemplate);
