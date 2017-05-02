4f885ec764ddf0f98034be79555beb0b1e657a58
vladpereskokov <vladpereskokov@mail.ru>
2017-05-02 09:01:05 +0300

    fix css about

--- public/views/About/About.tsx
+++ public/views/About/About.tsx
@@ -7,38 +7,42 @@ import './About.scss';
 
 class About extends React.Component<void, void> {
   render() {
-    const { isAuthenticated } = this.props;
+    const {isAuthenticated} = this.props;
 
     return (
-      <div className="about__wrapper-elements">
+      <div className="wrapper__about">
         <Background />
-        <div className="about__header">
-          <span>Soul Hunting</span>
+        <div className="about__wrapper-form">
+          <div className="about__form">
+            <div className="about__form-header">
+              <span>Soul Hunting</span>
+            </div>
+            { !isAuthenticated ?
+              browserHistory.push('/')
+              :
+              <form className="about__form-content" name="{data.title}">
+                <span className="developer">ANANYMOUS</span>
+                <p className="field__develop">FrontEnd</p>
+                <hr />
+                <ul>
+                  <li className="author__name">
+                    <label>Pereskokov Vladislav</label>
+                  </li>
+                  <li className="author__name">
+                    <label>Artyuhov Vladislav</label>
+                  </li>
+                </ul>
+                <p className="field__develop">BackEnd</p>
+                <hr />
+                <ul>
+                  <li className="author__name">
+                    <label>Nabokov Denis</label>
+                  </li>
+                </ul>
+              </form>
+            }
+          </div>
         </div>
-        { !isAuthenticated ?
-          browserHistory.push('/')
-          :
-          <form className="about" name="{data.title}">
-            <span className="developer">ANANYMOUS</span>
-            < p className="field__develop">FrontEnd</p>
-            <hr />
-            <ul>
-              <li className="author__name">
-                <label>Pereskokov Vladislav</label>
-              </li>
-              <li className="author__name">
-                <label>Artyuhov Vladislav</label>
-              </li>
-            </ul>
-            <p className="field__develop">BackEnd</p>
-            <hr />
-            <ul>
-              <li className="author__name">
-                <label>Nabokov Denis</label>
-              </li>
-            </ul>
-          </form>
-        }
       </div>
     );
   }
