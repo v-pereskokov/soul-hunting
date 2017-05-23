982c03cdc226e73881f18fc21d8dc3460e7d963c
vladpereskokov <vladpereskokov@mail.ru>
2017-05-23 13:42:38 +0300

    add mimificator

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,7 +17,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'babel-loader!awesome-typescript-loader'
       },
 
       {
