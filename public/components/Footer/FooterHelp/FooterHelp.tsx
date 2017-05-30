82efd4dc4af432842467c0dab40d63b1352d8138
vladpereskokov <vladpereskokov@mail.ru>
2017-05-30 23:34:30 +0300

    delete selector from footer

--- public/components/Footer/FooterHelp/FooterHelp.tsx
+++ public/components/Footer/FooterHelp/FooterHelp.tsx
@@ -4,8 +4,6 @@ export class FooterHelp extends React.Component<any, any> {
   render() {
     return (
       <div className='footer__wrapper__help'>
-        <img className='footer__help-img' src='../../static/images/arrows.png'/>
-        <p className='footer__help-selector'>Control</p>
         <img className='footer__help-img footer__help-img-enter' src='../../static/images/enter_button.png'/>
         <p className='footer__help-enter'>Select</p>
       </div>
